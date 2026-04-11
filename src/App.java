1d26a7d5eebcc7679ba11dfbef832c7576704f6a
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-04-11 19:28:41 +0300

    feat: app file completed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import db.DatabaseManager;
+import panels.MainFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+
+        DatabaseManager.initDatabase();
+
+        SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
     }
 }
