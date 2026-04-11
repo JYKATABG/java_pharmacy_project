6874656ff035bdf74036225574d53a277a1f82ae
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-04-11 19:14:11 +0300

    feat: add main frame

--- src/panels/MainFrame.java
+++ src/panels/MainFrame.java
@@ -1,5 +1,21 @@
 package panels;
 
-public class MainFrame {
-    
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        setTitle("Аптека - Управление");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000, 650);
+        setLocationRelativeTo(null);
+
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.addTab("Лекарства", new MedicationPanel());
+        tabs.addTab("Клиенти", new ClientPanel());
+        tabs.addTab("Продажби", new SalePanel());
+
+        add(tabs);
+    }
 }
